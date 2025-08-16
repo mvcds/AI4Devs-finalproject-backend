534f48f2727bb958ee7b48a8838446a7b14b5854
Marcos Silva <marvic.silva@gmail.com>
2025-08-16 11:52:01 +0200

    allow test environemnt files

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { Category } from './domain/entities/category.entity'
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env.local', '.env'],
+      envFilePath: ['.env.local', '.env', '.env.test'],
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
